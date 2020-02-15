aac0b11a589eccfab9b71e9fc5a554e665b33276
김태형 <taehyeong224@gmail.com>
2020-02-15 12:58:01 +0900

    delete removee method

--- lambda/index.js
+++ lambda/index.js
@@ -12,30 +12,6 @@ exports.handler = async (event) => {
     return `Successfully processed ${event.Records.length} messages.`;
 };
 
-/**
- * SQS에서 받은 메시지를 삭제한다.
- **/
-const deleteMessages = (url, messages) => {
-    console.log("delete : ")
-    if (messages.Messages === undefined || messages.Messages === null) {
-        return;
-    }
-
-    // SQS 삭제에 필요한 형식으로 변환한다.
-    const entries = messages.Messages.map((msg) => {
-        return {
-            Id: msg.MessageId,
-            ReceiptHandle: msg.ReceiptHandle,
-        };
-    });
-
-    // 메시지를 삭제한다.
-    return sqs.deleteMessageBatch({
-        Entries: entries,
-        QueueUrl: url,
-    }).promise();
-}
-
 const sendRequest = async ({userId, message, createdAt}) => {
     try {
         await axios({
